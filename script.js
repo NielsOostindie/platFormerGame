62d5c2d3f2e8f90299056e9ef7e5aab37ccf2b48
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2023-02-03 11:25:09 +0100

    beta

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const img = new Image();
+img.onload = () => {
+  
+}
+img.src = './assets/playerSrpite.png';
 const canvas = document.querySelector("canvas");
 
 //*declare what gametype it is
@@ -5,85 +10,170 @@ const c = canvas.getContext("2d");
 
 //*set canvas to the whole page
 canvas.width = 1920;
-canvas.height = 1080;
+canvas.height = 977;
 
 //*sets gravity value to add overtime
-const gravity = 0.6;
+const gravity = 0.5;
 
 //*make a class for the player
 class Player {
   constructor() {
     //!give the Player a x, y, width and height
     this.postion = {
-      x: 250,
-      y: 1050,
+      x: 300,
+      y: 600,
     };
     //!Give the Player gravity
     this.velocity = {
       x: 0,
       y: 1,
     };
-    this.width = 30;
-    this.height = 30;
+    this.width = 37;
+    this.height = 70;
 
     this.isJumping = true;
   }
 
   //*draw the Player
   draw() {
-    c.fillStyle = "red";
-    c.shadowColor = "red";
-    c.shadowBlur = 12;
-    c.shadowOffsetX = 0;
-    c.shadowOffsetY = 0;
-    c.fill();
-    c.fillRect(this.postion.x, this.postion.y, this.width, this.height);
+    if(img){
+      c.save();
+      c.drawImage(img, this.postion.x, this.postion.y);
+      c.restore();
+    }
   }
+
   //*updates the Player every time
   update() {
     //!adds the gravity(velocity.y) the the y postion of the Player
     this.postion.y += this.velocity.y;
     this.postion.x += this.velocity.x;
     //*monitoring the bottom of Player
-    if (this.postion.y + this.height + this.velocity.y <= canvas.height)
+    if (this.postion.y + this.height + this.velocity.y <= canvas.height) {
       //*makes the gravity accelerate over time
       this.velocity.y += gravity;
+    }
     //*if the bottom of the Player hits the bottom of the Canvas,
     //*stop putting gravity on it
-    else {
-      this.velocity.y = 0;
-      player.isJumping = false;
-    }
   }
 }
 
-class Platform {
+class Npc {
+  constructor() {
+    this.postion = {
+      x: 450,
+      y: 790,
+    };
+
+    this.width = 30;
+    this.height = 60;
+  }
+
+  draw() {
+    c.fillStyle = "green";
+    c.fill();
+    c.fillRect(this.postion.x, this.postion.y, this.width, this.height);
+
+    c.fillStyle = "black";
+    c.font = "17px sans-serif";
+    c.fillText("Talk to the me!", this.postion.x, this.postion.y);
+  }
+}
+
+class Spike {
   constructor(x, y) {
     this.postion = {
       x,
       y,
     };
-    this.width = 250;
-    this.height = 40;
+
+    this.width = 5;
+    this.height = 50;
+    this.coords = [90, -120, 60, 60, 120, 60];
   }
 
   draw() {
-    c.fillStyle = "blue";
-    c.shadowColor = " ";
-    c.shadowBlur = 0;
-    c.shadowOffsetX = 0;
-    c.shadowOffsetY = 0;
+    c.save();
+    c.fillStyle = "red";
+    c.beginPath();
+    c.moveTo(this.postion.x + this.coords[0], this.postion.y + this.coords[1]);
+    c.lineTo(this.postion.x + this.coords[2], this.postion.y + this.coords[3]);
+    c.lineTo(this.postion.x + this.coords[4], this.postion.y + this.coords[5]);
+    c.fill();
+    c.restore();
+  }
+}
+
+class Platform {
+  constructor(x, y, w, h) {
+    this.postion = {
+      x,
+      y,
+    };
+    this.width = w;
+    this.height = h;
+  }
+
+  draw() {
+    c.save();
+    c.fillStyle = "black";
     c.fill();
     c.fillRect(this.postion.x, this.postion.y, this.width, this.height);
+    c.restore();
   }
 }
 
 const player = new Player();
+const npc = new Npc();
 const platforms = [
-  new Platform(800, 600),
-  new Platform(500, 800),
-  new Platform(500, 380),
+  //*border
+  new Platform(-400, -100, 400, 1920),
+  new Platform(2400, -900, 800, 8020),
+  new Platform(0, -800, 2400, 400),
+  //*platforms
+  new Platform(0, 850, 1350, 300),
+  new Platform(1550, 725, 250, 30),
+  new Platform(1550, 220, 250, 30),
+  new Platform(2020, 450, 250, 30),
+  new Platform(880, 15, 250, 30),
+
+  //*end
+  new Platform(0, -100, 650, 30),
 ];
+const spikes = [
+  new Spike(1270, 977),
+  new Spike(1310, 977),
+  new Spike(1350, 977),
+  new Spike(1390, 977),
+  new Spike(1430, 977),
+  new Spike(1470, 977),
+  new Spike(1510, 977),
+  new Spike(1550, 977),
+  new Spike(1590, 977),
+  new Spike(1630, 977),
+  new Spike(1670, 977),
+  new Spike(1710, 977),
+  new Spike(1750, 977),
+  new Spike(1790, 977),
+  new Spike(1830, 977),
+  new Spike(1870, 977),
+  new Spike(1910, 977),
+  new Spike(1950, 977),
+  new Spike(1990, 977),
+  new Spike(2030, 977),
+  new Spike(2070, 977),
+  new Spike(2110, 977),
+  new Spike(2150, 977),
+  new Spike(2190, 977),
+  new Spike(2230, 977),
+  new Spike(2270, 977),
+  new Spike(2310, 977),
+  new Spike(2350, 977),
+  new Spike(2390, 977),
+  new Spike(22270, 977),
+  
+];
+
 //!define the keys we want to monitor
 const keys = {
   right: {
@@ -104,6 +194,10 @@ function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
   player.draw();
+  npc.draw();
+  spikes.forEach((spike) => {
+    spike.draw();
+  });
   platforms.forEach((platform) => {
     platform.draw();
   });
@@ -113,26 +207,73 @@ function animate() {
   //*if the Player x axis has had the hav way mark stop moving
   if (keys.right.pressed && player.postion.x < window.innerWidth / 2) {
     player.velocity.x = 8;
-  } else if (keys.left.pressed && player.postion.x > 200) {
+  } else if (keys.left.pressed && player.postion.x > 300) {
     player.velocity.x = -8;
   } else {
     player.velocity.x = 0;
     if (keys.right.pressed) {
+      npc.postion.x -= 8;
       platforms.forEach((platform) => {
         platform.postion.x -= 8;
       });
+      spikes.forEach((spike) => {
+        spike.postion.x -= 8;
+      });
     }
     if (keys.left.pressed) {
+      npc.postion.x += 8;
       platforms.forEach((platform) => {
         platform.postion.x += 8;
       });
+      spikes.forEach((spike) => {
+        spike.postion.x += 8;
+      });
     }
   }
-  if (keys.space.pressed && player.postion.y < 350) {
+
+  if (keys.space.pressed && player.postion.y < 310) {
+    npc.postion.y += 10;
     platforms.forEach((platform) => {
-      platform.postion.y += 20;
+      platform.postion.y += 10;
+    });
+    spikes.forEach((spike) => {
+      spike.postion.y += 10;
     });
   }
+
+  // if (player.postion.y < 350 && player.velocity.y > 1) {
+  //   npc.postion.y -= 50;
+  //   platforms.forEach((platform) => {
+  //     platform.postion.y -= 50;
+  //   });
+  //   spikes.forEach((spike) => {
+  //     spike.postion.y -= 50;
+  //   });
+  // }
+
+  if (player.velocity.y > 21) {
+    npc.postion.y -= 30;
+    platforms.forEach((platform) => {
+      platform.postion.y -= 30;
+    });
+    spikes.forEach((spike) => {
+      spike.postion.y -= 30;
+    });
+  }
+
+  spikes.forEach((spike) => {
+    if (
+      spike.postion.y + spike.height >= player.postion.y &&
+      spike.postion.y <= player.postion.y + player.height &&
+      spike.postion.x + spike.width >= player.postion.x &&
+      spike.postion.x <= player.postion.x + player.width
+    ) {
+      window.location.reload();
+      player.velocity.y = 0;
+    }
+  });
+
+
 
   platforms.forEach((platform) => {
     //*platform collision detection
@@ -143,7 +284,9 @@ function animate() {
       player.postion.x + player.width >= platform.postion.x &&
       player.postion.x <= platform.postion.x + platform.width
     ) {
-      player.isJumping = false;
+      if ((player.isJumping = false)) {
+        // window.location.reload();
+      }
       player.velocity.y = 0;
     }
 
